86647daffabf0a85ca021cff9abc95f6d9926b62
Brian <cvj4b2ypcq@privaterelay.appleid.com>
2026-07-23 23:34:27 +0200

    Wis-knop terug; datumregel uit gekopieerde tekst gehaald

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker voor Wachtboek
 // CACHE wordt automatisch ververst bij elke upload (upload.command bumpt dit getal).
-const CACHE = 'wachtboek-v20260723231801';
+const CACHE = 'wachtboek-v20260723233426';
 const CORE = [
   './',
   './index.html',
