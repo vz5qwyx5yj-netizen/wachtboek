403b466398762bc0f4a94e570fe87ba313508853
Brian Schlee <brianschlee@MacBook-Air-van-Brian.local>
2026-08-20 21:18:54 +0200

    Update 20-08-2026 21:18

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker voor Wachtboek
 // CACHE wordt automatisch ververst bij elke upload (upload.command bumpt dit getal).
-const CACHE = 'wachtboek-v20260820204236';
+const CACHE = 'wachtboek-v20260820211854';
 const CORE = [
   './',
   './index.html',
